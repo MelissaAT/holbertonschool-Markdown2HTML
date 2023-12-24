8f5e37c024cf8ac1efac47193139f753b0d7206a
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-12-24 13:45:33 -0400

    task 2

--- markdown2html.py
+++ markdown2html.py
@@ -12,7 +12,27 @@ def convert_headings(line):
     for i in range(6, 0, -1):
         if line.startswith('#' * i):
             return f"<h{i}>{line[i+1:].strip()}</h{i}>"
-    return line
+    return None
+
+def convert_lists(lines):
+    """Convert Markdown lists to HTML lists."""
+    in_list = False
+    html_lines = []
+    for line in lines:
+        if line.startswith('- '):
+            if not in_list:
+                html_lines.append('<ul>')
+                in_list = True
+            html_lines.append(f"<li>{line[2:].strip()}</li>")
+        else:
+            if in_list:
+                html_lines.append('</ul>')
+                in_list = False
+            if line.strip():  # Avoid adding blank lines
+                html_lines.append(line.strip())
+    if in_list:  # Close the list if the file ends while still in a list
+        html_lines.append('</ul>')
+    return html_lines
 
 def main():
     # Check if the number of arguments is less than 2
@@ -26,11 +46,24 @@ def main():
         print(f"Missing {markdown_file}", file=sys.stderr)
         exit(1)
 
-    # Open the Markdown file and the output HTML file
-    with open(markdown_file, 'r') as md_file, open(sys.argv[2], 'w') as html_file:
-        for line in md_file:
-            html_line = convert_headings(line)
-            html_file.write(html_line + '\n')
+    # Read the Markdown file and convert it to HTML
+    with open(markdown_file, 'r') as md_file:
+        lines = md_file.readlines()
+
+    html_lines = []
+    for line in lines:
+        heading = convert_headings(line)
+        if heading is not None:
+            html_lines.append(heading)
+        else:
+            html_lines.append(line)
+
+    html_lines = convert_lists(html_lines)
+
+    # Write the HTML output
+    with open(sys.argv[2], 'w') as html_file:
+        for line in html_lines:
+            html_file.write(line + '\n')
 
     exit(0)
 
